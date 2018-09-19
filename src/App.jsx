5f4e5ee591609ef9e47be86a9e41a019ebb0c124
Robert Yuen <dungeonry@hotmail.com>
2018-09-18 23:43:53 -0700

    added axios and login error cases.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import axios from "axios";
 import Form from "./Form.jsx";
 
 class App extends Component{
@@ -27,22 +28,51 @@ class App extends Component{
 
     handleFormSubmit = e => {
         e.preventDefault();
-        console.log("Logging in.");        
-        this.setState(prevState => {
-            return {
-                ...prevState,
-                username: "",
-                password: ""
-            };
-        });
+        console.log("Logging in.");       
+        const url = "http://localhost:5789/login";
+        const username = this.state.username;
+        const password = this.state.password;
+
+        axios.post(url, {
+            username: username,
+            password: password
+        }).then(res => {
+            console.log(res)
+            this.setState(prevState => {
+                return {
+                    ...prevState,
+                    username: "", 
+                    password: ""
+                };
+            })
+            window.location.assign("http://localhost:8080");
+        }).catch(err => {
+            if(err.response.status === 401){
+                console.log("Incorrect password.")
+                this.setState(prevState => {
+                    return {
+                        ...prevState,
+                        password: ""
+                    };
+                });
+            }
+            if(err.response.status === 404){
+                console.log("User does not exist.");
+                this.setState(prevState => {
+                    return {
+                        ...prevState,
+                        username: "",
+                        password: ""
+                    };
+                });
+            }
+        });       
     }
     
     render(){                
         return (
             <div className = "root">    
-                <h2>Royu static</h2>  
-                <h2>Cats are the best.</h2>  
-                <h2>Dogs are okay though</h2>                           
+                <h2>Login</h2>                                             
                 <Form 
                     onInputChange = {this.handleInputChange}
                     usernameValue = {this.state.username}
